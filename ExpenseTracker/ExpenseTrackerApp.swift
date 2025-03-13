fc49394e883a902b3bb51d6ecfa59ffeb5c7cc18
Darshan Bodara <57811313+darshan6122@users.noreply.github.com>
2025-03-13 17:25:15 -0400

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Darshan Bodara on 2025-03-13.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
